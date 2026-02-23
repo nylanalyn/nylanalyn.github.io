95edc4767121a56e2c5c93c20d056cdd9eda8e3a
nylan <nylan@nylan.cat>
2026-02-23 17:55:04 -0500

    indieweb h-card

---
+++ copy_img.py
@@ -0,0 +1,4 @@
+import shutil
+import glob
+src = glob.glob("/home/nylan/.gemini/antigravity/brain/*/crayon_cat_head_*.png")[0]
+shutil.copy(src, "/home/nylan/code/nylanalyn.github.io/images/nylan-picture.png")
